d3dd6ae0746994b1196059c120fbcb8d80a1516a
acmb <48370302+acmb@users.noreply.github.com>
2024-06-27 16:28:10 +0100

    Update http-test.js

--- spec/tests/http-test.js
+++ spec/tests/http-test.js
@@ -21,41 +21,11 @@ it('should request via axios', async ({ $, t }) => {
   t.equal(typeof result.headers, 'object')
   t.deepStrictEqual(result.data, { method: 'GET' })
 
-  result = await $.http({ ...request, method: 'post' })
+  result = await $.http({ ...request, method: 'post', data: 'test' })
   t.equal(Object.keys(result).length, 3)
   t.equal(result.status, 200)
   t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, { method: 'POST' })
-
-  result = await $.http({ ...request, method: 'put' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 200)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, { method: 'PUT' })
-
-  result = await $.http({ ...request, method: 'patch' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 200)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, { method: 'PATCH' })
-
-  result = await $.http({ ...request, method: 'delete' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 200)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, { method: 'DELETE' })
-
-  result = await $.http({ ...request, method: 'options' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 204)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, '')
-
-  result = await $.http({ ...request, method: 'head' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 200)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, '')
+  t.deepStrictEqual(result.data, { method: 'POST', body: 'test' })
 })
 
 it('should request via socks5', async ({ $, t }) => {
@@ -81,41 +51,11 @@ it('should request via socks5', async ({ $, t }) => {
   t.equal(typeof result.headers, 'object')
   t.deepStrictEqual(result.data, { method: 'GET' })
 
-  result = await $.http({ ...request, method: 'post' })
+  result = await $.http({ ...request, method: 'post', data: 'test' })
   t.equal(Object.keys(result).length, 3)
   t.equal(result.status, 200)
   t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, { method: 'POST' })
-
-  result = await $.http({ ...request, method: 'put' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 200)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, { method: 'PUT' })
-
-  result = await $.http({ ...request, method: 'patch' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 200)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, { method: 'PATCH' })
-
-  result = await $.http({ ...request, method: 'delete' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 200)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, { method: 'DELETE' })
-
-  result = await $.http({ ...request, method: 'options' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 204)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, '')
-
-  result = await $.http({ ...request, method: 'head' })
-  t.equal(Object.keys(result).length, 3)
-  t.equal(result.status, 200)
-  t.equal(typeof result.headers, 'object')
-  t.deepStrictEqual(result.data, '')
+  t.deepStrictEqual(result.data, { method: 'POST', body: 'test' })
 })
 
 xit('should use different IP address via socks5', async ({ $, t }) => {
@@ -136,6 +76,60 @@ xit('should use different IP address via socks5', async ({ $, t }) => {
   t.notEqual(axiosIp, socks5Ip)
 })
 
+it('should request with aliases', async ({ $, t }) => {
+  var request = { url: 'http://localhost:9000/test' }
+
+  var result = await $.http.get('http://localhost:9000')
+  t.equal(Object.keys(result).length, 3)
+  t.equal(result.status, 404)
+  t.equal(typeof result.headers, 'object')
+  t.deepStrictEqual(result.data, {
+    error: 'AxiosError: Request failed with status code 404'
+  })
+
+  result = await $.http.get(request.url)
+  t.equal(Object.keys(result).length, 3)
+  t.equal(result.status, 200)
+  t.equal(typeof result.headers, 'object')
+  t.deepStrictEqual(result.data, { method: 'GET' })
+
+  result = await $.http.post(request.url, { data: 'test' })
+  t.equal(Object.keys(result).length, 3)
+  t.equal(result.status, 200)
+  t.equal(typeof result.headers, 'object')
+  t.deepStrictEqual(result.data, { method: 'POST', body: 'test' })
+
+  result = await $.http.put(request.url)
+  t.equal(Object.keys(result).length, 3)
+  t.equal(result.status, 200)
+  t.equal(typeof result.headers, 'object')
+  t.deepStrictEqual(result.data, { method: 'PUT' })
+
+  result = await $.http.patch(request.url)
+  t.equal(Object.keys(result).length, 3)
+  t.equal(result.status, 200)
+  t.equal(typeof result.headers, 'object')
+  t.deepStrictEqual(result.data, { method: 'PATCH' })
+
+  result = await $.http.delete(request.url)
+  t.equal(Object.keys(result).length, 3)
+  t.equal(result.status, 200)
+  t.equal(typeof result.headers, 'object')
+  t.deepStrictEqual(result.data, { method: 'DELETE' })
+
+  result = await $.http.options(request.url)
+  t.equal(Object.keys(result).length, 3)
+  t.equal(result.status, 204)
+  t.equal(typeof result.headers, 'object')
+  t.deepStrictEqual(result.data, '')
+
+  result = await $.http.head(request.url)
+  t.equal(Object.keys(result).length, 3)
+  t.equal(result.status, 200)
+  t.equal(typeof result.headers, 'object')
+  t.deepStrictEqual(result.data, '')
+})
+
 it('should retry', async ({ $, t }) => {
   async function clean() {
     return $.http('http://localhost:9000/test/retry/clean')
@@ -152,6 +146,10 @@ it('should retry', async ({ $, t }) => {
   await clean()
 
   var result = await $.http({ ...request, retries: 2 })
+  t.equal(result.status, 200)
+  await clean()
+
+  var result = await $.http.get(request.url, { retries: 2 })
   t.equal(result.status, 200)
   await clean()
 })
