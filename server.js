03970a2df5c90bfb4b57e3eed1b02be9dbbeaede
acmb <48370302+acmb@users.noreply.github.com>
2024-06-27 15:38:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 var furu = require('furu')
 
-furu({ port: 9000 }, async function (req) {
-  if (req.pathname == '/test') {
-    return { method: req.method }
+var retries = 0
+
+furu({ port: 9000 }, async function (req, res) {
+  switch (req.pathname) {
+    case '/test':
+      return { method: req.method }
+    case '/test/retry':
+      console.log({ retries })
+      if (retries < 2) {
+        retries++
+        res.statusCode = 500
+        return ''
+      } else {
+        retries = 0
+        return { method: req.method }
+      }
+    case '/test/retry/clean':
+      retries = 0
+      return ''
+    default:
+      return
   }
 })
